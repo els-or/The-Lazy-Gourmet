0ad4c9b6a2bbbc5578a9dacfc3ac53660325a4e3
lunledev <183235731+lunledev@users.noreply.github.com>
2025-02-06 19:15:12 -0800

    added history & association

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,10 +1,12 @@
 import sequelize from '../config/connection.js';
 import { UserFactory } from './user.js';
 import { RecipeFactory } from './recipe.js';
+import { HistoryFactory } from './history.js';
 
 //init the models
 const User = UserFactory(sequelize);
 const Recipe = RecipeFactory(sequelize);
+const History = HistoryFactory(sequelize);
 
 
 //create associations between models
@@ -16,6 +18,13 @@ User.hasMany(Recipe, { //user having many recipes.
 
 Recipe.belongsTo(User);
 
+User.hasMany(History, { //user having many history.
+
+    onDelete: 'CASCADE',
+});
+
+History.belongsTo(User);
 
 
-export { sequelize, User, Recipe };
+
+export { sequelize, User, Recipe, History };
