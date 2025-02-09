c8de3c67a08d04c200d23617bb00044a8a23b522
Rachel Sutton <raesak@gmail.com>
2025-02-09 15:22:49 -0800

    update display and routes

--- client/src/components/DisplayRecipe.tsx
+++ client/src/components/DisplayRecipe.tsx
@@ -1,8 +1,38 @@
 export default function DisplayRecipe(props: any) {
+  if (!props.recipe) {
+    return (
+      <div>
+        No recipe to display please submit a request or lookup a recipe from
+        your previous requests.
+      </div>
+    );
+  }
+  let recipe = props.recipe.message;
+  console.log(recipe);
+  recipe = recipe.replaceAll("```", "");
+  recipe = recipe.replace("json", "");
+  recipe = JSON.parse(recipe);
   return (
     <div>
-      <h3>Recipe</h3>
-      {JSON.stringify(props.recipe)}
+      <h3>{recipe.recipeTitle}</h3>
+      <h4>Ingredients</h4>
+      <ul>
+        {recipe.ingredients.map((ingredient: string, index: number) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h4>Cooking Steps</h4>
+      <ul>
+        {recipe.cooking.map((step: string, index: number) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ul>
+      <h4>Serving Instructions</h4>
+      <p>{recipe.serving}</p>
+      <h4>Dessert</h4>
+      <p>{recipe.dessert}</p>
+      <h4>Drink</h4>
+      <p>{recipe.drink}</p>
     </div>
   );
 }
