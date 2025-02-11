5efe7dea87bf8013a281259c90242039580365a2
Rachel Sutton <raesak@gmail.com>
2025-02-10 16:50:23 -0800

    fix to background size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-background-primary h-fit text-text font-body">
+    <div className="bg-background-primary min-h-screen h-full text-text font-body">
       <Navbar />
       <main className="container pt-5">
         <Outlet />
